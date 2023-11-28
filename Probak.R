541ce4bd2806ff8c0931758c8179d23bbc39bb0b
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-11-28 11:38:45 +0100

    Próbák végrehajtása

--- Probak.R
+++ Probak.R
@@ -2,3 +2,10 @@
 apropos("test")
 ## Vizualizálás
 boxplot(pH1, pH2, ylab = "pH")
+## F-próba a szórások összehasonlításához
+?var.test
+## F-próba végrehajtása a két adatsoron
+var.test(pH1, pH2)
+## t-próba egyező varianciákhoz
+?t.test
+t.test(pH1, pH2)
